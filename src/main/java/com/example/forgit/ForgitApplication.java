d0eaa75f3e3ec139e4701b7653af4730b6d3841d
New-Oleg <kokosic9@mail.ru>
2024-11-06 20:38:06 +0500

    проект создан

---
+++ src/main/java/com/example/forgit/ForgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.forgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forgit/ForgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
